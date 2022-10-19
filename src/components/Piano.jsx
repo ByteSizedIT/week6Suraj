75ee956e9df6b7a73c9f6e686c930d0bf8b76d3d
patdel0 <zefiru@protonmail.com>
2022-10-19 17:47:59 +0100

    Add score

--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -2,13 +2,14 @@ import Key from "./Key"
 import { useEffect, useState } from "react"
 import { NOTES, keyMap } from "./constants"
 
-const Piano = () => {
+const Piano = ({ setScore }) => {
   const [selectedNote, setSelectedNote] = useState("")
 
   function handleKeyDown(event) {
     for (let key in keyMap) {
       if (event.key === key) {
         setSelectedNote(keyMap[event.key])
+        setScore((currentScore) => currentScore + 1)
       }
     }
   }
