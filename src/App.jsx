6d43aa3fec5122bf1b16a5ac5778b75c453e57f0
Suraj Pun <s_pun@outlook.com>
2022-10-19 12:09:27 +0100

    Added notes and missing keys

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import './App.css'
 import BlackKey from './BlackKeys'
 import WhiteKey from "./WhiteKey";
 
+// Keys 1
+const notes = ['C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4'];
+
+// Keys 2
+const notes2 = ['C5', 'Db5', 'D5', 'Eb5', 'E5', 'F5', 'Gb5', 'G5', 'Ab5', 'A5', 'Bb5', 'B5'];
 
 function App() {
   return (
